7697c44a4541eb029856ed8c5d498072ff4f68c6
Aditya Kumar <aditya.work404@gmail.com>
2026-04-11 20:49:39 +0530

    added sent message section in UI

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,7 +2,8 @@ import { useEffect } from 'react'
 import { useState,useRef } from 'react'
 
 const App = () => {
-  const [message , setMessage] = useState(['hi there' , 'hello there'])
+  const [message , setMessage] = useState(['hi there' , 'hello there','how are you all , good'])
+  const [sentMsg , setSentmsg] = useState(['i am good ', 'hows your day'])
 
   const wsRef = useRef()
   const inputRef = useRef(null)
@@ -27,6 +28,8 @@ const App = () => {
 
   function clickHandler(){
     const msg = inputRef.current.value;
+    setSentmsg([...sentMsg,msg])
+
     wsRef.current.send(JSON.stringify({
       type :'chat',
       payload: {
@@ -36,20 +39,37 @@ const App = () => {
   }
 
   return <>
-    <div className='flex flex-col h-screen bg-black text-white '>
-      <br />
-      <br />
-      <div className='h-[90vh]'>
-        {message.map((e) =>{ 
-          return <div className='m-3'> 
-           <span className='border rounded p-3  bg-white text-black'>{e}</span>
-           </div>
-          })}
-      </div>
-      <div className='flex'>
-        <input ref={inputRef} type="text" placeholder='Type your msg...' />
-        <button onClick={clickHandler} className=''>Send</button>
-      </div>
+    <div className=' border-2 h-screen bg-black  '>
+        <div className='flex border-2 h-[90vh]'>
+          <div className='LEFT w-1/2  border bg-black'>
+            <div className='border-2  border-amber-500 m-7'>
+              {message.map((e,i) => {
+                return <div className='m-2 border flex  border-red-500'>
+                    <span className='border-2 rounded bg-white  p-3 text-black' key={i}>{e}</span>
+                </div>
+              })}
+            </div>
+
+          </div>
+
+          <div className='RIGHT w-1/2 bg-rose-500'>
+              <div className='border-2  border-amber-500 m-7'>
+                {sentMsg.map((e,i) => {
+                  return <div className='m-2 border flex justify-end'>
+                    <span className='border-2 rounded  bg-white  p-3 text-black' key={i}>{e}</span>
+                    </div>
+                })}
+              </div>
+
+          </div>
+        </div>
+        <div className='ml-96 mr-96 h-16 text-white'>
+          <div className='flex mt-2  '>
+            <input className='bg-white caret-black p-1 w-[40vw] text-gray-700 h-12' ref={inputRef} type="text" placeholder='Type your msg...' />
+            <button onClick={clickHandler} className='border ml-5 rounded-xl bg-blue-500 w-24'>Send</button>
+          </div>
+        </div>
+
     </div>
   </>
 }
