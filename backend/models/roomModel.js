4a746f3d66fb261264a1384c423a99ff82f6c077
Aditya Kumar <aditya.work404@gmail.com>
2026-04-15 17:54:22 +0530

    created roomSchema

---
+++ backend/models/roomModel.js
@@ -0,0 +1,18 @@
+import mongoose from "mongoose";
+
+const roomSchema = new mongoose.Schema({
+    name : {
+        type : String,
+        required : true
+    },
+    members : [
+        {
+            type : mongoose.Schema.Types.ObjectId,
+            ref : 'user'
+        }
+    ]
+})
+
+const room = mongoose.model('room',roomSchema)
+
+export default room
