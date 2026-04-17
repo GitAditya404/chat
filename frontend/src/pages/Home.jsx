7ce0f18c9e4a3057759d3f4ba5d211bf78644bf4
Aditya Kumar <aditya.work404@gmail.com>
2026-04-18 01:50:21 +0530

    ui for joining room

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -19,24 +19,36 @@ const Home = () => {
 
   return (
     <>
-    <div className='OUTER flex w-full h-screen bg-amber-500'>
+    <div className='OUTER flex w-full text-white h-screen bg-black'>
 
-        <div className='SIDEBAR flex flex-col border-2 w-1/3 px-7 py-2'>
+        <div className='SIDEBAR flex flex-col border w-1/3 px-7 py-2'>
 
             <div className='flex border items-start'>
-                <h1 className='border  text-3xl'>Connect</h1>
+                <h1 className='border text-white font-bold  text-3xl'>Connect</h1>
                 <button className='border ml-auto rounded bg-blue-600 text-white p-2'>Profile</button>
             </div>
-            <div className='border-4 flex-1 bg-red-600 '>
-               { rooms.map((room) => {
+            <div className='border flex-1 bg-red-600 '>
+               {/* { rooms.map((room) => {
                     <h1>room</h1>
-                })}    
+                })}     */}
             </div>
 
         </div>
 
-        <div className='RIGHT border bg-rose-500 w-2/3'>
+        <div className='RIGHT flex justify-center items-center border  w-2/3'>
+                <div className='rounded-xl w-[40vw] h-60 border-2 text-white '>
 
+                  <div className='text-center '>
+                    <p className='text-4xl font-bold'>Join a Room</p>
+                  </div>
+
+                  <div className='ml-5'>
+                    <p className='text-xl mt-9'>Room Name</p>
+                    <input className='block mt-1 bg-white text-black rounded p-3' type="text" placeholder='name' />
+                    <button className='border mt-4 text-xl rounded bg-blue-600 text-white py-3 px-8' type="submit">Join</button>
+                  </div>
+
+                </div>
         </div>
     </div>
     </>
