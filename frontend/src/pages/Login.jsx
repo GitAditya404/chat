3b8e3230c8f269adbfff7d548638bb052d4b9936
Aditya Kumar <aditya.work404@gmail.com>
2026-04-19 09:48:52 +0530

    added  login logic

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,9 +1,23 @@
+import axios from 'axios'
+import { useRef } from 'react'
+import { useNavigate } from 'react-router-dom'
+
 const Login = () => {
+    const emailRef = useRef(null)
+    const passwordRef = useRef(null)
+    const navigate = useNavigate();
+    
+    async function loginHandler() {
+        const resp = await axios.post('http://localhost:3000/login',
+            {
+                email: emailRef.current.value,
+                password : passwordRef.current.value 
+            },
+            {withCredentials: true}
+        )
+        navigate('/')
 
-    function loginHandler() {
-        
     }
-
 
 
     return <>
@@ -12,14 +26,16 @@ const Login = () => {
                 <h3 className="text-3xl text-semibold ">Hello,</h3>
                 <h3 className="text-3xl text-semibold mb-5">Welcome Back</h3>
                 <p className="tracking-tight">Enter your email and password to access your account</p>
-                <form className="w-full mt-8"  >
-                    <input className="px-3 py-2  w-3/5 outline-none bg-[#3C364C] border-2 border-zinc-800 rounded-md" type="text" name="email" placeholder="email" />
+                <div className="w-full mt-8"  >
+                    <input ref={emailRef} className="px-3 py-2  w-3/5 outline-none bg-[#3C364C] border-2 border-zinc-800 rounded-md" type="text" name="email" placeholder="email" />
 
-                    <input className="px-3 mt-5 py-2 w-3/5 outline-none bg-[#3C364C] border-2 border-zinc-800 rounded-md" type="text" name="password" placeholder="password" />
+                    <input ref={passwordRef} className="px-3 mt-5 py-2 w-3/5 outline-none bg-[#3C364C] border-2 border-zinc-800 rounded-md" type="text" name="password" placeholder="password" />
+
                     <button onClick={loginHandler} className="px-3 block mt-8 py-2 bg-blue-500 rounded-md">Login</button>
+
                     <p className="tracking-tight mt-8 inline-block">Don't have an account? </p>
                     <a className="text-blue-500 underline ml-2  mt-8" href="/signup">Sign up</a>
-                </form>
+                </div>
             </div>
 
             <div className="w-1/2 h-full  rounded-xl overflow-hidden">
