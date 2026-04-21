781438af968f366f6e81934b9ba726e20503080b
Aditya Kumar <aditya.work404@gmail.com>
2026-04-21 15:50:59 +0530

    feature : added webSocketServer code

--- backend/index.js
+++ backend/index.js
@@ -66,51 +66,54 @@ app.post('/room/create' , isLoggedIn ,async (req,res) =>{
 })
 
 app.listen(3000)
-// const wss = new WebSocketServer({port:8080})  
 
-//     let allSocket = {}
+                                  //WEBSOCKET SERVER
+const wss = new WebSocketServer({port:8080})  
 
-//     wss.on("connection" , (socket) => {  //whenever there is a connectn to this webSocketServer call this fn and give it a socket
-//         console.log('user Connected')
+    let allSocket = {}
 
-//         socket.on("message" , (msg) => {
-//             const parsedMsg = JSON.parse(msg)  // since msg that is coming is string u need to conver it to obj
+    wss.on("connection" , (socket) => {  //whenever there is a connectn to this webSocketServer call this fn and give it a socket
+        console.log('user Connected')
 
-//             if(parsedMsg.type === 'join')
-//             {
-//                 const roomId = parsedMsg.payload.roomId;
-//                 if(!allSocket[roomId]){
-//                     allSocket[roomId] = [];
-//                 }
-//                 allSocket[roomId].push(socket);
-//                 console.log('user joined')
-//                 socket.room = roomId  //attaching a room field in socket itself , so that later the room of the socket can be identified instantly
-//             }
+        socket.on("message" , (msg) => {
+            const parsedMsg = JSON.parse(msg)  // since msg that is coming is string u need to conver it to obj
 
-//             if(parsedMsg.type === 'chat')
-//             {
-//                 const roomId = socket.room;
-//                 if(!roomId) // check if socket has a room 
-//                     return console.log('user not in any room')
-//                 if(!allSocket[roomId])  // check if the room exist in our server
-//                     return console.log('room doesnt exist')
+
+            if(parsedMsg.type === 'join')
+            {
+                const roomId = parsedMsg.payload.roomId;
+                if(!allSocket[roomId]){
+                    allSocket[roomId] = [];
+                }
+                allSocket[roomId].push(socket);
+                console.log('user joined')
+                socket.room = roomId  //attaching a room field in socket itself , so that later the room of the socket can be identified instantly
+            }
+
+            if(parsedMsg.type === 'chat')
+            {
+                const roomId = socket.room;
+                if(!roomId) // check if socket has a room 
+                    return console.log('user not in any room')
+                if(!allSocket[roomId])  // check if the room exist in our server
+                    return console.log('room doesnt exist')
                 
-//                 const clients = allSocket[roomId]
+                const clients = allSocket[roomId]
 
-//                 const msgWithTime = {
-//                     msg: parsedMsg.payload.msg,
-//                     time : new Date()
-//                 }
+                const msgWithTime = {
+                    msg: parsedMsg.payload.msg,
+                    time : new Date()
+                }
 
-//                 clients.forEach((c) => {
-//                     if(c!=socket){
-//                         c.send(JSON.stringify(msgWithTime))
-//                     }
-//                 })   
-//             }
-//         })
+                clients.forEach((c) => {
+                    if(c!=socket){
+                        c.send(JSON.stringify(msgWithTime))
+                    }
+                })   
+            }
+        })
 
-//         // socket.on("disconnect", () => {
-//         //     allSocket = allSocket.filter(x => x != socket)
-//         // })
-// })
+        // socket.on("disconnect", () => {
+        //     allSocket = allSocket.filter(x => x != socket)
+        // })
+})
