bcd2ba381eaf7633af8165bbe359a7dd2b7185d6
Aditya Kumar <aditya.work404@gmail.com>
2026-05-03 01:06:42 +0530

    feat: create multerConfig to handle file request

---
+++ backend/config/multerConfig.js
@@ -0,0 +1,10 @@
+import multer from "multer";
+
+const storage  = multer.memoryStorage() // keep temporary in RAM when file uploaded
+
+const upload = multer({  // upload is function that will process incoming file requests
+    storage: storage,  //when file comes where should multer keep the file (memory storage)
+    limits : {fileSize : 5*1024*1024}  //5mb limit
+})
+
+export default upload
