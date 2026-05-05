3a41f66453e5a821ee8c304390c06d92ad30500a
Aditya Kumar <aditya.work404@gmail.com>
2026-05-05 12:35:25 +0530

    feature : created global websocket context

---
+++ frontend/context/WebSocketContext.jsx
@@ -0,0 +1,28 @@
+import { createContext, useContext, useEffect, useState } from "react";
+
+export const WsContext = createContext() ;
+
+export function WsProvider ({children}){
+    const [ws , setWs]  = useState(null);
+
+    useEffect(() => {
+        const socket = new WebSocket(import.meta.env.VITE_WS_URL)
+
+        socket.onopen = () => { 
+            console.log("WebSocket connection is now ready")
+        }
+        setWs(socket)
+
+        return () => {
+            ws.close()
+        }
+
+    },[])
+
+
+    return (
+        <WsContext.Provider value={ws}>
+            {children}
+        </WsContext.Provider>
+    )
+}
