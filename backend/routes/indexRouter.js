5aa7ae66bde3990f52cd33f3cde0d15730bf2583
Aditya Kumar <aditya.work404@gmail.com>
2026-05-08 17:48:39 +0530

    feat : endpoint to create meeting

--- backend/routes/indexRouter.js
+++ backend/routes/indexRouter.js
@@ -1,65 +1,75 @@
 import express from 'express'
 const router = express.Router()
+import { google } from "googleapis"
 
 import roomModel from '../models/roomModel.js'
 import isLoggedIn from '../middlewares/isLoggedIn.js'
 
-// let online = []
 router.get('/me' , isLoggedIn , (req,res) => {
-  // online.push(req.user._id)
   res.status(200).send('user verified')
 })
-
-// router.get('/status', isLoggedIn ,async (req,res) => {
-//   const {roomId} = req.query;
-//   const user = req.user._id;
-
-//   try{
-
-//     const room = await roomModel.findOne({_id:roomId})
-//     if(!room)
-//       return res.status(404).json({
-//         msg : "Room not found"
-//     })
-
-
-//     if(room.members.length>2)
-//       return res.status(403).json({
-//         msg : "Status not applicable"
-//     })
-
-//     online.forEach((id) =>{
-//       if(id.toString() === room.members[0].toString() && id.toString() != user.toString())
-//         return res.status(200).json({
-//           msg : "Online"
-//       })
-
-//       if(id.toString() === room.members[1].toString() && id.toString() != user.toString())
-//         return res.status(200).json({
-//           msg : "Online"
-//       })
-
-//     })
-
-//     return res.status(200).json({
-//       msg : "Offline"
-//     })
-
-//   }
-  
-//   catch(err){
-//     return res.status(500).json({
-//       msg : "Internal Server Error"
-//     })
-//   }
-  
-// })
 
 router.get('/health' , (req,res) => {
   res.status(200).json({
     status : "OK" , 
     time : new Date()
   })
+})
+
+router.post('/createMeeting', isLoggedIn ,async (req,res) => {
+
+    try{
+        const { title, startTime, endTime, emails} = req.body
+        const user = req.user;
+
+        const oauth2Client = new google.auth.OAuth2()
+
+        oauth2Client.setCredentials({
+            access_token: user.googleAccessToken
+        })
+
+        const calendar = google.calendar({
+            version: "v3",
+            auth: oauth2Client
+        })
+
+        const event = await calendar.events.insert({
+
+            calendarId: "primary",
+
+            sendUpdates: "all",
+
+            requestBody: {
+
+                summary: title,
+
+                start: {
+                    dateTime: startTime + ":00",
+                    timeZone: "Asia/Kolkata"
+                },
+
+                end: {
+                    dateTime: endTime + ":00",
+                    timeZone: "Asia/Kolkata"
+                },
+
+                attendees: emails.map(email => ({email}))
+            }
+        })
+
+        return res.status(200).json({
+          msg : "Meeting scheduled successfully"
+        })
+    }
+
+    catch(err){
+        console.log(err)
+
+        return res.status(500).json({
+            msg : "Meeting creation failed"
+        })
+    }
+
 })
 
 export default router
